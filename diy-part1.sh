3608e803e9c55d58a9483f0136bad6e161fd75be
ioo88ooi <59564436+ioo88ooi@users.noreply.github.com>
2022-01-19 14:27:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
-git clone https://github.com/fw876/helloworld package/lean/helloworld
-#git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
+#git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
+#git clone https://github.com/fw876/helloworld package/lean/helloworld
+git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
